adae2e5e397a497589266a8cc02731ba995cce8b
tianxuanxuan <xx_tian189@163.com>
2020-09-18 17:53:18 +0800

    add Config-client模块

---
+++ boot-starter-config-client3355/src/main/java/com/xgit/boot/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.xgit.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by tianxuanxuan
+ * On 2020-09-18 16:25
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
